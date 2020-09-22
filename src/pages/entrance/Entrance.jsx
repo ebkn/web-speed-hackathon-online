a6f18b3a2608641879120ae2a12c033764f359f6
ebkn <ktennis.mqekr12@gmail.com>
2020-09-22 16:47:56 +0900

    feat: remove lodash

--- src/pages/entrance/Entrance.jsx
+++ src/pages/entrance/Entrance.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import _ from 'lodash';
 import Helmet from 'react-helmet';
 
 import { renderNotFound } from '../../domains/error/error_actions';
@@ -78,7 +77,7 @@ export function Entrance() {
   }
 
   if (pickups.length === 0 && blogList.length !== 0) {
-    setPickups(_.chain(blogList).take(10).shuffle().take(4).value());
+    setPickups(blogList.slice(0, 10).sort(() => Math.random() - 0.5).slice(0, 4));
   }
 
   return (
