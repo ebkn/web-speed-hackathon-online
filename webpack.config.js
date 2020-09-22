6aa73486e772e08f4be992b2f166ce62e8f077e8
ebkn <ktennis.mqekr12@gmail.com>
2020-09-22 13:59:30 +0900

    feat: use preact as alias

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
 
   resolve: {
     extensions: ['.js', '.jsx'],
+    alias: {
+      react: 'preact/compat',
+      'react-dom': 'preact/compat',
+    },
   },
 
   plugins: [
