9f91b4e305d91d81b91c44a8202818966ad8582d
ebkn <ktennis.mqekr12@gmail.com>
2020-09-22 15:54:19 +0900

    feat: use file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ module.exports = {
       {
         test: /\.(png|svg|jpe?g|gif|webp)$/,
         use: {
-          loader: 'url-loader',
+          loader: 'file-loader',
         },
       },
     ],
